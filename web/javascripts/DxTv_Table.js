a8bd2627b2bc2207019ca8e6d13e366d87b68488
Maria Barrem <maria.souza@dextra-sw.com>
2015-02-03 14:53:10 -0200

    Tabela pronta e arquivos separados

---
+++ web/javascripts/DxTv_Table.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+  var createTable = function () {
+    var quantidade = 20;
+    var tabela = $('#ultimos-commits');
+
+    for (var i = 0; i < quantidade; i++) {
+      var tr = $('<tr>');
+      tr.append($('<td>').addClass('n' + i));
+      tabela.append(tr);
+    }
+
+    setInterval(function() {
+      $.ajax({
+        url: "http://dx-pointz.appspot.com/api/transactions?q={'order':[{p:'date',d:'desc'}],limit:" + quantidade + "}",
+        crossDomain : true,
+      }).done(function(transactions) {
+        $.each(transactions, function(i, transaction) {
+          $.ajax({
+            url: 'http://dx-pointz.appspot.com/api' + transaction.personId,
+            dataType : 'json',
+            crossDomain : true
+          }).done(function (person) {
+            tabela.find('td.n' + i).html(person.name);
+          });
+        });
+      });
+    }, 2000);
+  };
+  createTable();
+});
