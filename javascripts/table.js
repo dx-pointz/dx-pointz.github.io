6c10327fde1498cf4cb2abd31f36f4bcb0d1d378
Luan Nico <luanpotter28@hotmail.com>
2015-02-15 22:45:20 -0200

    refactoring table.js

--- javascripts/table.js
+++ javascripts/table.js
@@ -1,48 +1,30 @@
 $(document).ready(function () {
+  var quantidade = 5;
 
-  // var createTable = function () {
-     var quantidade = 20;
-  //   var tabela = $('#ultimos-commits');
-
-  //   // for (var i = 0; i < quantidade; i++) {
-  //   //   var tr = $('<tr>');
-  //   //   tr.append($('<td>').addClass('n' + i));
-  //   //   tabela.append(tr);
-  //   //}
-  //   var commitsDinamicos = $(".commitersList");
-  //   //var json = [{name: "Julio Carvalho", type: "GITHUB_COMMIT"},{name: "Luan Nico", type: "GITHUB_COMMIT"},
-  //   //{name: "Sabrina", type: "GITHUB_COMMIT"}, {name: "Guilherme Carreiro", type: "GITHUB_COMMIT"}, {name: "Antonio Vianna", type: "GITHUB_COMMIT"}];
-  //   //json.forEach(function(obj){
-  //   //  commitsDinamicos.append("<li><a class='ls-ico-user'>"+ obj.name +" <span class='ls-tag ls-float-right'>"+
-  //                                   obj.type+"</span></a></li>");
-
-  //   });
-   
-    //setInterval(function() {
-      //var json = [];
-
-      $.ajax({
-        url: "http://dx-pointz.appspot.com/api/transactions?q={'order':[{p:'date',d:'desc'}],limit:" + quantidade + "}",
+  var performRequest = function (cardCreator) {
+    $.ajax({
+        url: 'http://dx-pointz.appspot.com/api/transactions?q={order:[{p:"date",d:"desc"}],limit:' + quantidade + '}',
         crossDomain : true,
-      }).done(function(transactions) {
-        var commitsDinamicos = $(".commitersList");
-        $.each(transactions, function(i, transaction) {
-          $.ajax({
-            url: 'http://dx-pointz.appspot.com/api' + transaction.personId,
-            dataType : 'json',
-            crossDomain : true
-          }).done(function (person) {
-            //tabela.find('td.n' + i).html(person.name);
-            //json.push({"name": person.name, "type": transaction.type});//FIXME FIXME FIXME PLS
-            commitsDinamicos.append("<li><a class='ls-ico-user'>"+ person.name +" <span class='ls-tag ls-float-right'>"+
-                                    transaction.type+"</span></a></li>");
-
-         
-           });
+    }).done(function (transactions) {
+      $.each(transactions, function (i, transaction) {
+        $.ajax({
+          url: 'http://dx-pointz.appspot.com/api' + transaction.personId,
+          dataType : 'json',
+          crossDomain : true
+        }).done(function (person) {
+          transaction.person = person;
+          cardCreator(transaction);
         });
       });
-    //}, 20000);
-  //};
+    });
+  };
 
-  //createTable();
+  var createCard = function (transaction) {
+    var list = $('.commitersList');
+    var type = $('<span>').addClass('ls-tag ls-float-right').text(transaction.type);
+    var a = $('<a>').addClass('ls-ico-user').text(transaction.person.name).append(type);
+    list.append($('<li>').append(a));
+  };
+
+  performRequest(createCard);
 });
